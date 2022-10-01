1e722c3f2300c2ebc84371cbbc75a62d233c8a3e
erenkucuk <erenkkucuk89@gmail.com>
2022-10-01 03:23:23 -1000

    adding static import for Assertions class

--- src/test/java/Day1/TestLifeCycleAnnotations.java
+++ src/test/java/Day1/TestLifeCycleAnnotations.java
@@ -5,14 +5,34 @@ import org.junit.jupiter.api.*;
 @DisplayName("Learning Life Cycle Annotations")
 public class TestLifeCycleAnnotations {
 
+    @BeforeAll
+    public static void init(){
+        System.out.println("Before all is running");
+    }
+
+    @AfterAll
+    public static void close(){
+        System.out.println("After all is running");
+    }
+
+    @BeforeEach
+    public void initEach(){
+        System.out.println("Before each is running");
+    }
+
+    @AfterEach
+    public void tearDownEach(){
+        System.out.println("After each is running");
+    }
+
     @Test
     public void test1(){
-
+        System.out.println("Test 1 is running");
     }
 
     @Test
     public void test2(){
-
+        System.out.println("Test 2 is running");
     }
 
 }
