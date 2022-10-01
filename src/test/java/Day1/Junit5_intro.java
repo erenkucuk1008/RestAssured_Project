fe024d81e0982c187c7f41808849fbdc854d065e
erenkucuk <erenkkucuk89@gmail.com>
2022-10-01 01:46:23 -1000

    adding static import for Assertions class

--- src/test/java/Day1/Junit5_intro.java
+++ src/test/java/Day1/Junit5_intro.java
@@ -2,10 +2,14 @@ package Day1;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class Junit5_intro {
 
     @Test
     public void test(){
         System.out.println("Learning Junit5 now");
+
+        assertEquals(1,2);
     }
 }
