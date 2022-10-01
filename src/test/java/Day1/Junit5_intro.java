3fe2c1c06f66f0f1dfeb3f397eefefd649b888fb
erenkucuk <erenkkucuk89@gmail.com>
2022-10-01 03:11:48 -1000

    adding static import for Assertions class

--- src/test/java/Day1/Junit5_intro.java
+++ src/test/java/Day1/Junit5_intro.java
@@ -1,15 +1,29 @@
 package Day1;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayName("Day1 of Junit5 Test")
 public class Junit5_intro {
 
+    @DisplayName("Testing numbers")
     @Test
     public void test(){
         System.out.println("Learning Junit5 now");
 
-        assertEquals(1,2);
+        assertEquals(1,1);
+        //assertEquals(1,2,"Somethig is wrong!!");
+    }
+
+    @DisplayName("Testing name start with A")
+    @Test
+    public void my_name_first_character(){
+
+        String firstName = "Adnan";
+        assertEquals('A',firstName.charAt(0));
+        //assertTrue(firstName.startsWith("A"));
+
     }
 }
---
+++ src/test/java/Day1/TestLifeCycleAnnotations.java
@@ -0,0 +1,18 @@
+package Day1;
+
+import org.junit.jupiter.api.*;
+
+@DisplayName("Learning Life Cycle Annotations")
+public class TestLifeCycleAnnotations {
+
+    @Test
+    public void test1(){
+
+    }
+
+    @Test
+    public void test2(){
+
+    }
+
+}
