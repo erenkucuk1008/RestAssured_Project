84863656e8cea7588fd97734023a96891c30bab0
erenkucuk <erenkkucuk89@gmail.com>
2022-10-03 03:10:57 -1000

    created test_util package for base class

--- src/test/java/Day1/BreakingBadPractice.java
+++ src/test/java/Day1/BreakingBadPractice.java
@@ -11,19 +11,32 @@ import static org.hamcrest.Matchers.*;
 
 public class BreakingBadPractice extends BreakingBadBaseTest {
 
-    @DisplayName("Breaking Bad Get Request")
+    @DisplayName("Breaking Bad Get All Characters")
     @Test
     public void getAllCharacters(){
-        get("/characters").prettyPeek();
+        Response response = get("/characters").prettyPeek();
+        assertThat(response.statusCode(), is(200));
     }
 
-    @DisplayName("Get 1 Character")
+    @DisplayName("Breaking Bad Get 1 Character")
     @Test
     public void getOneCharacter(){
-
-        Response response = get("/characters/1");
-
+        Response response = get("/characters/1").prettyPeek();
         assertThat(response.statusCode(), is(200));
         System.out.println("response.path(\"name\") = " + response.path("name"));
+    }
+
+    @DisplayName("Breaking Bad Get All Episodes")
+    @Test
+    public void getAllEpisodes(){
+        Response response = get("/episodes").prettyPeek();
+        assertThat(response.statusCode(), is(200));
+    }
+
+    @DisplayName("Breaking Bad Get One Episode")
+    @Test
+    public void getOneEpisode(){
+        Response response = get("/episodes/16").prettyPeek();
+        assertThat(response.statusCode(), is(200));
     }
 }
