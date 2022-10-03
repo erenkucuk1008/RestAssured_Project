26715d30acb7a0cb4866384b438cbe536250e2f0
erenkucuk <erenkkucuk89@gmail.com>
2022-10-03 01:37:55 -1000

    added new class

---
+++ src/test/java/Day1/SpartanNoAuthTest.java
@@ -0,0 +1,21 @@
+package Day1;
+
+import org.junit.jupiter.api.*;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+@DisplayName("Spartan App Get Requests")
+public class SpartanNoAuthTest {
+
+    //http://3.91.182.164:8000/api/spartans
+
+    @Test
+    public void getAllSpartans(){
+
+
+    }
+}
