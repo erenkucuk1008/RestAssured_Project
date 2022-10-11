7a63ac1e8750f8be608c2fffd57c9e3e0d58e2fc
erenkucuk <erenkkucuk89@gmail.com>
2022-10-10 23:06:34 -1000

    created classes

--- src/test/java/Day3/SpartanPostingData_Test.java
+++ src/test/java/Day3/SpartanPostingData_Test.java
@@ -9,6 +9,7 @@ import io.restassured.response.Response;
 import org.junit.jupiter.api.*;
 import test_util.SpartanNoAuthBaseTest;
 
+import java.io.File;
 import java.util.List;
 import java.util.Map;
 
@@ -40,11 +41,43 @@ public class SpartanPostingData_Test extends SpartanNoAuthBaseTest{
                 .log().all()
 //                .header("Content-Type", "application/json")
 //                .contentType("application/json")
-                .contentType(ContentType.JSON)
+                .contentType(ContentType.JSON) // this is providing header for request
                 .body(postStrBody).
         when()
-                .post("/spartans");
-        //then()
+                .post("/spartans").
+        then()
+                .log().all()
+                .statusCode(is(201))
+                .contentType(ContentType.JSON) // this is asserting response header is json
+                .body("success",is("A Spartan is Born!"))
+                .body("data.name", is("Eren"))
+                .body("data.id", is(131))
+        ;
+    }
 
+    @DisplayName("POST /spartans with external file")
+    @Test
+    public void testPostDataWithJsonFileAsBody(){
+
+        /* singleSpartan.json with below content
+            {
+                "name" : "Eren",
+                "gender" : "Male",
+                "phone" : 8083494130
+            }
+         */
+
+        File jsonFile = new File("singleSpartan.json");
+
+        given()
+                .log().all()
+                .contentType(ContentType.JSON)
+                .body(jsonFile).
+        when()
+                .post("/spartans").
+        then()
+                .log().all()
+                .statusCode(201)
+                ;
     }
 }
